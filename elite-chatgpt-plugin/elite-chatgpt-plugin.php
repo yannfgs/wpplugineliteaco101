3532c0fa65f852a39f8c0602ccdce7ccb338ed3f
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-06-16 16:32:36 -0300

    first commit

---
+++ elite-chatgpt-plugin/elite-chatgpt-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: EliteAço ChatGPT Plugin
+Plugin URI: https://www.eliteaco.com.br
+Description: Este é um plugin que conecta o ChatGPT com o meu site WordPress.
+Version: 1.0
+Author: Yann Szilagyi
+Author URI: https://www.eliteaco.com.br
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; //Exit if accessed directly
+}
+
+class 
