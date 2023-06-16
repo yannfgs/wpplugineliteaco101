a6673d3d4f108a873eed17a3dd5f577a7fa178df
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-06-16 17:15:33 -0300

    16/06/23_php_02

--- elite-chatgpt-plugin/elite-chatgpt-plugin.php
+++ elite-chatgpt-plugin/elite-chatgpt-plugin.php
@@ -12,4 +12,42 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; //Exit if accessed directly
 }
 
-class 
+class EliteAco_ChatGPT_Plugin {
+
+    public function __construct() {
+        add_action('init', array($this, 'init'));
+    }
+
+    public function init() {
+        // O código de inicialização do plugin vai aqui
+    }
+
+    public function get_chatgpt_response($prompt) {
+        $api_url = 'https://api.openai.com/v1/engines/davinci-codex/completions'; // URL da API
+        $api_key = 'sua_api_key_aqui'; // Substitua por sua chave de API
+    $headers = array(
+        'Authorization' => 'Bearer' . $api_key,
+        'Content-Type' => 'application/json'
+    );
+    $body = array(
+        'prompt' => $prompt,
+        'max_tokens' => 60,
+    );
+
+    $args = array(
+        'headers' => $headers,
+        'body' => json_encode($body),
+        'method' => 'POST',
+        'data_format' => 'body',
+    );
+
+    $http = new WP_Http();
+    $response = $http->request($api_url, $args);
+
+    if (is_wp_error($response)) {
+        // Trate o erro aqui
+    } else {
+        return json_decode($response['body']);
+    }
+  }
+}
