2ec63c0d6208de83d768bb2c7c20d2de783debcc
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-06-17 09:16:13 -0300

    17/06/23_php_02

--- elite-chatgpt-plugin/elite-chatgpt-plugin.php
+++ elite-chatgpt-plugin/elite-chatgpt-plugin.php
@@ -1,15 +1,15 @@
 <?php
 /*
-Plugin Name: EliteAço ChatGPT Plugin
-Plugin URI: https://www.eliteaco.com.br
+Plugin Name: EliteAco ChatGPT Plugin
+Plugin URI: https://www.eliteaco.com
 Description: Este é um plugin que conecta o ChatGPT com o meu site WordPress.
 Version: 1.0
-Author: Yann Szilagyi
-Author URI: https://www.eliteaco.com.br
+Author: Seu Nome
+Author URI: https://www.eliteaco.com
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit; //Exit if accessed directly
+    exit; // Exit if accessed directly
 }
 
 class EliteAco_ChatGPT_Plugin {
@@ -19,37 +19,52 @@ class EliteAco_ChatGPT_Plugin {
     }
 
     public function init() {
-        // O código de inicialização do plugin vai aqui
+        add_shortcode('chatgpt_form', array($this, 'chat_form_shortcode'));
     }
 
     public function get_chatgpt_response($prompt) {
         $api_url = 'https://api.openai.com/v1/engines/davinci-codex/completions'; // URL da API
         $api_key = 'sua_api_key_aqui'; // Substitua por sua chave de API
-    $headers = array(
-        'Authorization' => 'Bearer' . $api_key,
-        'Content-Type' => 'application/json'
-    );
-    $body = array(
-        'prompt' => $prompt,
-        'max_tokens' => 60,
-    );
 
-    $args = array(
-        'headers' => $headers,
-        'body' => json_encode($body),
-        'method' => 'POST',
-        'data_format' => 'body',
-    );
+        $headers = array(
+            'Authorization' => 'Bearer ' . $api_key,
+            'Content-Type' => 'application/json'
+        );
 
-    $http = new WP_Http();
-    $response = $http->request($api_url, $args);
+        $body = array(
+            'prompt' => $prompt,
+            'max_tokens' => 60,
+        );
 
-    if (is_wp_error($response)) {
-        // Trate o erro aqui
-    } else {
-        return json_decode($response['body']);
+        $args = array(
+            'headers' => $headers,
+            'body' => json_encode($body),
+            'method' => 'POST',
+            'data_format' => 'body',
+        );
+
+        $http = new WP_Http();
+        $response = $http->request($api_url, $args);
+
+        if (is_wp_error($response)) {
+            // Trate o erro aqui
+        } else {
+            return json_decode($response['body']);
+        }
     }
-  }
+
+    public function chat_form_shortcode() {
+        ob_start();
+        ?>
+        <form id="chatgpt-form">
+            <label for="chatgpt-input">Faça uma pergunta:</label><br>
+            <input type="text" id="chatgpt-input" name="question" required><br>
+            <input type="submit" value="Enviar">
+        </form>
+        <div id="chatgpt-response"></div>
+        <?php
+        return ob_get_clean();
+    }
 }
 
 new EliteAco_ChatGPT_Plugin();
